2526d95fff154260d3ad220a6384e17ac6edb9ea
AkaGop <150795195+AkaGop@users.noreply.github.com>
2025-11-17 13:26:12 +0100

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -1,7 +1,9 @@
 # log_parser.py
 import re
 from io import StringIO
-from config import CEID_MAP, RPTID_MAP
+# MODIFIED: Changed to a relative import
+from .config import CEID_MAP, RPTID_MAP
+
 
 def _parse_s6f11_report(full_text: str) -> dict:
     data = {}
